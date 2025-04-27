ead0e74029fa070ab277dc25e87756cdc7b6a521
mutiarah-hikmah <mutiiarahkmh823@gmail.com>
2025-04-27 16:22:08 +0700

    change html and style

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      
+      targetElement.scrollIntoView({
         behavior: 'smooth'
+      });
+  
+      // Menambahkan animasi untuk CV Section
+      const items = document.querySelectorAll('.experience-item, .education-item');
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.style.animationPlayState = 'running';
+            observer.unobserve(entry.target);
+          }
+        });
+      }, {
+        threshold: 0.2 // Trigger saat 20% elemen kelihatan
+      });
+  
+      items.forEach(item => {
+        observer.observe(item);
+        item.style.animationPlayState = 'paused'; // Biar nunggu di-scroll dulu
       });
     });
   });
